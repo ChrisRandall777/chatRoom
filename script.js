54d751f0bf807058c9920649a504aa01f74b84aa
Chris Randall <cdr6@illinois.edu>
2025-01-22 17:04:15 -0600

    another try 2

--- script.js
+++ script.js
@@ -6,13 +6,16 @@
 //     methods: ["GET", "POST"]
 //   }
 //  })
-const socket = io.connect('http://2601:246:4d80:c080:91b3:bb96:300d:7b0b:3000', { transports : ['websocket']})
+const socket = io('http://2601:246:4d80:c080:91b3:bb96:300d:7b0b:3000', { transports : ['websocket'], cors : {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE, OPTIONS"
+}})
 
 var messageBox = document.getElementById('message')
 var messagesDiv = document.getElementById("messages")
 var tah = document.getElementById("textarea_holder")
 
-const username = prompt('What is your name?')
+const username = 'Chris' //prompt('What is your name?')
 appendMessage('Welcome ' + username + ' you\'ve joined the chat:)')
 
 socket.on('new-message', data => {
